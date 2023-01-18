9c6fddcd65eae0fb3c753759dc170a1cefe6295b
Ken Nwankwo <ken_nwankwo@shi.com>
2023-01-18 13:20:11 -0500

    Added HttpGet attribute

--- dotNet7-RPG/Controllers/CharacterController.cs
+++ dotNet7-RPG/Controllers/CharacterController.cs
@@ -9,6 +9,7 @@ namespace dotNet7_RPG.Controllers
    {
       private static Character knight = new Character();
 
+      [HttpGet]
       public IActionResult Get()
       {
          return Ok(knight);
