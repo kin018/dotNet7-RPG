e91bf7cd39d856e52d30d0adb3ae4e9b9d254a3e
Ken Nwankwo <ken_nwankwo@shi.com>
2023-01-18 13:04:10 -0500

    Added Comments

--- dotNet7-RPG/Models/Character.cs
+++ dotNet7-RPG/Models/Character.cs
@@ -2,5 +2,12 @@
 {
    public class Character
    {
+      public int Id { get; set; }
+      public string Name { get; set; } = "Frodo"; //public string? Name { get; set; } is the null-able version instead of hard coding in a value
+      public int HitPoints { get; set; } = 100;
+      public int Strength { get; set; } = 10;
+      public int Defense { get; set; } = 10;
+      public int Intelligence { get; set; } = 10;
+      public RpgClass Class { get; set; } = RpgClass.Knight;
    }
 }
--- dotNet7-RPG/Models/RpgClass.cs
+++ dotNet7-RPG/Models/RpgClass.cs
@@ -1,6 +1,9 @@
 ﻿namespace dotNet7_RPG.Models
 {
-   public class RpgClass
+   public enum RpgClass
    {
+      Knight = 1,
+      Mage = 2,
+      Cleric = 3,
    }
 }
