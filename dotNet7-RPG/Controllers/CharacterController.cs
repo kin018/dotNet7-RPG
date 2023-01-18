7e71ce549772a92421ac1ccc754be875806d6eb6
Ken Nwankwo <ken_nwankwo@shi.com>
2023-01-18 13:01:54 -0500

    Global using for Models added into Program.cs so I do not need to repetitively use the using statement on every controller etc.
    Added private static class

--- dotNet7-RPG/Controllers/CharacterController.cs
+++ dotNet7-RPG/Controllers/CharacterController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotNet7_RPG.Controllers
 {
    [ApiController]
-   [Route]
+   [Route("api/[controller]")]
    public class CharacterController : ControllerBase //since its just an API we use ControllerBase but if we were using a view we derive from controller
    {
-
+      private static Character knight = new Character();
    }
 }
--- dotNet7-RPG/Program.cs
+++ dotNet7-RPG/Program.cs
@@ -1,3 +1,5 @@
+global using dotNet7_RPG.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
