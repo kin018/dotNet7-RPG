7165a9139df8288375ddbbca10e855ec83675e6e
Ken Nwankwo <ken_nwankwo@shi.com>
2023-01-18 13:40:02 -0500

    Changed Controller Route

--- dotNet7-RPG/Controllers/CharacterController.cs
+++ dotNet7-RPG/Controllers/CharacterController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace dotNet7_RPG.Controllers
 {
    [ApiController]
-   [Route("api/[controller]")]
+   [Route("[controller]")]
    public class CharacterController : ControllerBase //since its just an API we use ControllerBase but if we were using a view we derive from controller
    {
       private static Character knight = new Character();
