0bee4c25e560b37aee9d6fb44a17a43ce1794bf8
Ken Nwankwo <ken_nwankwo@shi.com>
2023-01-23 11:10:46 -0500

    CharacterService

--- dotNet7-RPG/Services/CharacterService/CharacterService.cs
+++ dotNet7-RPG/Services/CharacterService/CharacterService.cs
@@ -1,6 +1,28 @@
-﻿namespace dotNet7_RPG.Services.CharacterService
+﻿using dotNet7_RPG.Models;
+
+namespace dotNet7_RPG.Services.CharacterService
 {
-   public class CharacterService
+   public class CharacterService : ICharacterService
    {
+      //Add Character List
+      private static List<Character> characters = new List<Character> {
+         new Character(),
+         new Character {Id = 1, Name = "Sam" },
+      };
+      public List<Character> AddCharacter(Character newCharacter)
+      {
+         characters.Add(newCharacter);
+         return characters;
+      }
+
+      public List<Character> GetAllCharacters()
+      {
+         return characters;
+      }
+
+      public Character GetCharacterById(int id)
+      {
+         return characters.FirstOrDefault(c => c.Id == id); //returns a character where the 1st Character id == given id
+      }
    }
 }
--- dotNet7-RPG/Services/CharacterService/ICharacterService.cs
+++ dotNet7-RPG/Services/CharacterService/ICharacterService.cs
@@ -2,5 +2,10 @@
 {
    public interface ICharacterService
    {
+      //Interface gets these 3 methods in essence the methods in character controller 
+      List<Character> GetAllCharacters(); //return a list of characters
+      List<Character> AddCharacter(Character newCharacter);//Return the list of characters
+      Character GetCharacterById(int id);//return single character 
+      
    }
 }
