511ae045a97ea72fc41e5745c21503437d035956
Ken Nwankwo <ken_nwankwo@shi.com>
2023-01-23 12:42:59 -0500

    .

--- dotNet7-RPG/Models/ServiceResponse.cs
+++ dotNet7-RPG/Models/ServiceResponse.cs
@@ -1,6 +1,9 @@
 ﻿namespace dotNet7_RPG.Models
 {
-   public class ServiceResponse
+   public class ServiceResponse<T> // T is actual type of data we want to return
    {
+      public T? Data { get; set; }
+      public bool Success { get; set; } = true;
+      public string Message { get; set; } = string.Empty;
    }
 }
--- dotNet7-RPG/Services/CharacterService/ICharacterService.cs
+++ dotNet7-RPG/Services/CharacterService/ICharacterService.cs
@@ -5,7 +5,7 @@
       //Interface gets these 3 methods in essence the methods in character controller
       Task<ServiceResponse<List<Character>>> GetAllCharacters(); //return a list of characters
 
-      Task<List<ServiceResponse<Character>>> AddCharacter(Character newCharacter);//Return the list of characters
+      Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);//Return the list of characters
 
       Task<ServiceResponse<Character>> GetCharacterById(int id);//return single character
    }
