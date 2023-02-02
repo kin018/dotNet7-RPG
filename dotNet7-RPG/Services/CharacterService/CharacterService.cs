1eac9ce528cc6ac3abd05391a6c17053bf4093fc
Ken Nwankwo <ken_nwankwo@shi.com>
2023-02-02 12:35:09 -0500

    Code Maid Cleanup

--- dotNet7-RPG/Services/CharacterService/CharacterService.cs
+++ dotNet7-RPG/Services/CharacterService/CharacterService.cs
@@ -98,7 +98,5 @@
 
          return serviceResponse;
       }
-
-
    }
 }
--- dotNet7-RPG/Services/CharacterService/ICharacterService.cs
+++ dotNet7-RPG/Services/CharacterService/ICharacterService.cs
@@ -9,6 +9,7 @@
       Task<ServiceResponse<List<GetCharacterDTO>>> AddCharacter(AddCharacterDTO newCharacter);//Return the list of characters
 
       Task<ServiceResponse<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO updatedCharacter);//return single character
+
       Task<ServiceResponse<List<GetCharacterDTO>>> DeleteCharacterById(int id);
    }
 }
